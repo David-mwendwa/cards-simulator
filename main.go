8c7d48a16df4bed1fee2f09e2a455affecc8ee1d
David Mwendwa <klatzdavie19@gmail.com>
2021-11-28 19:00:59 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for _, card := range cards {
+		fmt.Println(card)
+	}
+
+	
+}
+
+func newCard() string {
+	return "Five of Diamomds"
+}
